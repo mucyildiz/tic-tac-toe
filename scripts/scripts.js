d2bb3cf710938305dbf2589906d913181e202f4c
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-06-09 16:39:26 -0400

    added board and added render and updategameboard functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,30 @@
-const Formatter = (function() {
-    const log = (message) => console.log(message);
-
-
-    const makeUpperCase = (text) => {
-        log('hi');
-        return text.toUpperCase();
-    };
-
-    return {
-        makeUpperCase,
-    }
-})()
-console.log(Formatter.makeUpperCase('hi'));
-
 const GameBoard = (function() {
+    let gameContainer = document.querySelector('.game-container');
     let gameboard = [];
     for(let i=0; i<9; i++){
         gameboard.push('');
     }
     const render = () => {
-        // take in contents gameboard and return it as a 3 by 3 grid 
+        // take in contents gameboard and return it 9 divs
+        for(marker of gameboard){
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.innerHTML = marker;
+            gameContainer.appendChild(cell);
+        }
+
     }
 
     const updateGameBoard = (index, marker) => {
-        gameboard[index] = marker;
+        let targetCell = gameContainer.childNodes[index + 1];
+        targetCell.innerHTML = marker;
     }
 
     return{
-        render,
+        render, updateGameBoard,
     }
 })();
 
-GameBoard.accessKeyBoard();
+GameBoard.render();
+GameBoard.updateGameBoard(4, 'x');
+GameBoard.updateGameBoard(3, 'x');
