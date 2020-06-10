a633702513b4be3cfed4a07f49bae0ffb68d1c47
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-06-10 03:02:34 -0400

    tried to implement horizontal victory conditions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,27 +4,71 @@ const GameBoard = (function() {
     for(let i=0; i<9; i++){
         gameboard.push('');
     }
-    const render = () => {
-        // take in contents gameboard and return it 9 divs
-        for(marker of gameboard){
-            let cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.innerHTML = marker;
-            gameContainer.appendChild(cell);
-        }
 
+
+    for(marker of gameboard){
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.innerHTML = marker;
+        gameContainer.appendChild(cell);
     }
 
-    const updateGameBoard = (index, marker) => {
-        let targetCell = gameContainer.childNodes[index + 1];
-        targetCell.innerHTML = marker;
+    const updateGameBoard = () => {
+        // let targetCell = gameContainer.childNodes[index + 1];
+        //this.innerHTML = marker;
+        //console.log('active');
+        this.style.backgroundColor = 'red';
+        console.log('ac')
     }
 
     return{
-        render, updateGameBoard,
+        gameboard, gameContainer
     }
 })();
 
-GameBoard.render();
-GameBoard.updateGameBoard(4, 'x');
-GameBoard.updateGameBoard(3, 'x');
+const Player = (name, marker) => {
+    let gameflow = GameFlow();
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    const makePlay = (e) => {
+        if(e.target.innerHTML == ''){
+            e.target.innerHTML = getMarker();
+            gameflow.checkVictory(getMarker());
+        }
+    }
+
+    let cells = Array.from(document.querySelectorAll('.cell'));
+    cells.forEach(cell => cell.addEventListener('click', makePlay));
+
+    let cellContentArray = [];
+
+    return{getName, getMarker, makePlay}
+    
+};
+
+const GameFlow = () => {
+    //let playerOneMarker = playerOne.getMarker();
+    //let playerTwoMarker = playerTwo.getMarker();
+    let gameboard = Array.from(GameBoard.gameContainer.children);
+
+    const isMarker = (index, marker) => {
+        return gameboard[index] == marker;
+    }
+
+    const horizontalVictory = (marker) => {
+        return ((isMarker(0, marker) && isMarker(1, marker) && isMarker(2, marker)) || 
+        (isMarker(3, marker) && isMarker(4, marker) && isMarker(5, marker)) || 
+        (isMarker(6, marker) && isMarker(7, marker) && isMarker(8, marker))
+        )
+    }
+
+    const checkVictory = (marker) => {
+        console.log(horizontalVictory(marker));
+    }
+
+    return{checkVictory}
+};
+
+
+me = Player('yes', 'X');
