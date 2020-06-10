08a1cc0b99fd02c6054161cf27d6cfdead7dde22
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-06-10 16:35:19 -0400

    added logic to check for victory

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,14 +13,6 @@ const GameBoard = (function() {
         gameContainer.appendChild(cell);
     }
 
-    const updateGameBoard = () => {
-        // let targetCell = gameContainer.childNodes[index + 1];
-        //this.innerHTML = marker;
-        //console.log('active');
-        this.style.backgroundColor = 'red';
-        console.log('ac')
-    }
-
     return{
         gameboard, gameContainer
     }
@@ -34,6 +26,10 @@ const Player = (name, marker) => {
     const makePlay = (e) => {
         if(e.target.innerHTML == ''){
             e.target.innerHTML = getMarker();
+            for(i=0; i<GameBoard.gameContainer.children.length; i++){
+                // set gameboard array to match contents of tic tac toe board whenever a move is made
+                GameBoard.gameboard[i] = GameBoard.gameContainer.children[i].innerHTML;
+            }
             gameflow.checkVictory(getMarker());
         }
     }
@@ -52,20 +48,29 @@ const GameFlow = () => {
     //let playerTwoMarker = playerTwo.getMarker();
     let gameboard = Array.from(GameBoard.gameContainer.children);
 
-    const isMarker = (index, marker) => {
-        return gameboard[index] == marker;
+    const isMarker = (indexOne, indexTwo, indexThree, marker) => {
+        return gameboard[indexOne].innerHTML == marker && gameboard[indexTwo].innerHTML == marker && gameboard[indexThree].innerHTML == marker;
     }
 
     const horizontalVictory = (marker) => {
-        return ((isMarker(0, marker) && isMarker(1, marker) && isMarker(2, marker)) || 
-        (isMarker(3, marker) && isMarker(4, marker) && isMarker(5, marker)) || 
-        (isMarker(6, marker) && isMarker(7, marker) && isMarker(8, marker))
-        )
+        return (isMarker(0, 1, 2, marker) || isMarker(3, 4, 5, marker) || isMarker(6, 7, 8, marker))
     }
 
-    const checkVictory = (marker) => {
-        console.log(horizontalVictory(marker));
+    const verticalVictory = (marker) => {
+        return (isMarker(0, 3, 6, marker) || isMarker(1, 4, 7, marker) || isMarker(2, 5, 8, marker));
     }
+
+    const diagonalVictory = (marker) => {
+        return (isMarker(0, 4, 8, marker) || isMarker(2, 4, 6, marker));
+    }
+
+    //to justify all these if statements to my future self: 
+    //there's probably an algorithm for this but it would've taken longer to understand and implement
+    //there's only eight winning conditions so it's easier to just hard code it with if statements
+    const checkVictory = (marker) => {
+        console.log(horizontalVictory(marker) || verticalVictory(marker) || diagonalVictory(marker));
+    }
+
 
     return{checkVictory}
 };
