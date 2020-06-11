fb7004fba8cfcaf80cabf0598efbb39705bc2383
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-06-11 19:47:41 -0400

    added game logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,79 +1,83 @@
 const GameBoard = (function() {
     let gameContainer = document.querySelector('.game-container');
-    let gameboard = [];
+
     for(let i=0; i<9; i++){
-        gameboard.push('');
-    }
-
-
-    for(marker of gameboard){
         let cell = document.createElement('div');
         cell.className = 'cell';
-        cell.innerHTML = marker;
         gameContainer.appendChild(cell);
+    }
+    let gameArray = Array.from(gameContainer.children);
+
+    // when a turn is made, this function adds the marker of the player to the board
+    const updateGameBoard = (index, addedMarker) => {
+        if(gameArray[index].innerHTML == ''){
+            gameArray[index].innerHTML = addedMarker;
+        }
     }
 
     return{
-        gameboard, gameContainer
+        gameArray, updateGameBoard
     }
 })();
 
 const Player = (name, marker) => {
-    let gameflow = GameFlow();
     const getName = () => name;
     const getMarker = () => marker;
+    let winner = false;
 
-    const makePlay = (e) => {
-        if(e.target.innerHTML == ''){
-            e.target.innerHTML = getMarker();
-            for(i=0; i<GameBoard.gameContainer.children.length; i++){
-                // set gameboard array to match contents of tic tac toe board whenever a move is made
-                GameBoard.gameboard[i] = GameBoard.gameContainer.children[i].innerHTML;
-            }
-            gameflow.checkVictory(getMarker());
-        }
-    }
 
-    let cells = Array.from(document.querySelectorAll('.cell'));
-    cells.forEach(cell => cell.addEventListener('click', makePlay));
 
-    let cellContentArray = [];
-
-    return{getName, getMarker, makePlay}
+    return{getName, getMarker, winner}
     
 };
 
 const GameFlow = () => {
-    //let playerOneMarker = playerOne.getMarker();
-    //let playerTwoMarker = playerTwo.getMarker();
-    let gameboard = Array.from(GameBoard.gameContainer.children);
+    let playerOne = Player('me', 'X');
+    let playerTwo = Player('you', 'O');
+    let turnTracker = 0;
+    let gameboard = GameBoard.gameArray;
 
     const isMarker = (indexOne, indexTwo, indexThree, marker) => {
         return gameboard[indexOne].innerHTML == marker && gameboard[indexTwo].innerHTML == marker && gameboard[indexThree].innerHTML == marker;
     }
-
     const horizontalVictory = (marker) => {
         return (isMarker(0, 1, 2, marker) || isMarker(3, 4, 5, marker) || isMarker(6, 7, 8, marker))
     }
-
     const verticalVictory = (marker) => {
         return (isMarker(0, 3, 6, marker) || isMarker(1, 4, 7, marker) || isMarker(2, 5, 8, marker));
     }
-
     const diagonalVictory = (marker) => {
         return (isMarker(0, 4, 8, marker) || isMarker(2, 4, 6, marker));
     }
-
-    //to justify all these if statements to my future self: 
-    //there's probably an algorithm for this but it would've taken longer to understand and implement
-    //there's only eight winning conditions so it's easier to just hard code it with if statements
     const checkVictory = (marker) => {
-        console.log(horizontalVictory(marker) || verticalVictory(marker) || diagonalVictory(marker));
+        return horizontalVictory(marker) || verticalVictory(marker) || diagonalVictory(marker);
+    }
+
+    const makePlay = (index, currPlayer) => {
+        GameBoard.updateGameBoard(index, currPlayer.getMarker());
+        if(checkVictory(currPlayer.getMarker()) == true){
+            currPlayer.winner = true;
+            console.log(currPlayer.getName() + ' is win.')
+        };
     }
 
 
-    return{checkVictory}
+
+    gameboard.forEach(cell => cell.addEventListener('click', (e) => {
+        let index = gameboard.indexOf(e.target);
+        if(turnTracker == 0 && e.target.innerHTML == ''){
+            player = playerOne;
+            turnTracker = 1;
+        }
+        else if(turnTracker == 1 && e.target.innerHTML == ''){
+            player = playerTwo;
+            turnTracker = 0;
+        }
+        makePlay(index, player);
+    }))
+        
+
+    return{checkVictory, makePlay}
 };
 
-
-me = Player('yes', 'X');
+gameflow = GameFlow();
